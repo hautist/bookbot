dd0a6a32c4ded998e62c5b75300de37d7e0e8c7c
hautist <lookatnadeko@protonmail.com>
2025-06-08 20:30:11 +0100

    task 3: refactor code move word_count fn to new file

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+from stats import word_count
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
-
-
-def word_count(text):
-    words = text.split()
-    return len(words)
 
 
 def main():
