a20a1501ff28d02d8f358b7a9198ba9834007739
hautist <lookatnadeko@protonmail.com>
2025-06-08 20:51:37 +0100

    task 4: add fn to stats that counts how many times ech character appears

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, character_count_dict
 
 
 def get_book_text(filepath):
@@ -10,7 +10,9 @@ def get_book_text(filepath):
 def main():
     frankenstein_contents = get_book_text("books/frankenstein.txt")
     frankenstein_wc = word_count(frankenstein_contents)
+    frankenstein_char_dict = character_count_dict(frankenstein_contents)
     print(f"{frankenstein_wc} words found in the document")
+    print(frankenstein_char_dict)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def word_count(text):
     words = text.split()
     return len(words)
+
+
+def character_count_dict(text):
+    character_dict = {}
+    for char in text:
+        try:
+            character_dict[char.lower()] += 1
+        except KeyError:
+            character_dict[char.lower()] = 1
+    return character_dict
